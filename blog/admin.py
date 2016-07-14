a6b97844c8c6469eb62e9d3a536f6617fd9c0289
Diasell <gutsalyuk.taras@gmail.com>
2016-07-14 17:48:49 +0300

    added blog Model and Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BlogItemModel
+
+
+class BlogItemModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "updated", "created"]
+    list_filter = ["updated", "created"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = BlogItemModel
+
+
+
+admin.site.register(BlogItemModel, BlogItemModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,33 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+def upload_location(instance, filename):
+    return "%s/%s" % (instance.title, filename)
+
+
+class BlogItemModel(models.Model):
+
+    class Meta(object):
+        verbose_name = u"Blog Article"
+
+    author = models.ForeignKey(
+        User,
+    )
+
+    title = models.CharField(
+        max_length=255,
+        blank=False
+    )
+    title_image = models.ImageField(
+        upload_to=upload_location,
+        blank=True,
+        verbose_name=u"Cover Image"
+    )
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
