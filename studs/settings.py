cb11e361be20cc12f781e2ccb91856a1723fa194
Diasell <gutsalyuk.taras@gmail.com>
2016-07-22 17:29:55 +0300

    ALLOWED_HOSTS - ANY

--- studs/settings.py
+++ studs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rk#qb-*3gi&ervt_ufoc05k3p#v*#viz&jg+p)v+fzo&0mx#(c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
