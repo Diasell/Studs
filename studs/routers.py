42c27bdcd1901f84c6b063687b79b2fdc11b5738
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-08-24 23:03:55 +0300

    Added custom router for APIViews

---
+++ studs/routers.py
@@ -0,0 +1,50 @@
+from django.conf.urls import url
+
+from students.util import HybridRouter
+from students.api import apiv1 as apiv1s
+from professors.api import  apiv1 as apiv1p
+from blog.api import apiv1 as apiv1b
+
+
+
+router = HybridRouter()
+
+# viewsets:
+#router.register(r'App Users', apiv1s.UserViewSet)
+#router.register(r'App Permission Groups', apiv1s.GroupViewSet)
+#router.register(r'Students', apiv1s.StudentViewSet)
+#router.register(r'BLOG', apiv1b.BlogViewSet)
+
+# APIViews:
+router.add_api_view("User Login view",
+                    url(r'^auth/login/$',
+                        apiv1s.LoginAPIView.as_view(),
+                        name='login-view'))
+router.add_api_view("User REGISTRATION",
+                    url(r'^auth/register/$',
+                    apiv1s.RegisterAPIView.as_view(),
+                    name='register-view'))
+router.add_api_view("User Schedule for Today",
+                    url(r'^get_user_schedule/$',
+                        apiv1s.TodayScheduleView.as_view(),
+                        name='today-schedule-view'))
+router.add_api_view("User Schedule for current week",
+                   url(r'^get_user_weekly_schedule/$',
+                       apiv1s.WeeklyScheduleView.as_view(),
+                       name='weekly-schedule-view'))
+router.add_api_view("Show all the students for the given group",
+                   url(r'^get_students_group_list/$',
+                       apiv1s.GroupStudentListView.as_view(),
+                       name='list-group_students-view'))
+router.add_api_view("Show student discipline results by date range",
+                    url(r'^get_st_dsp_result/$',
+                        apiv1s.StudentClassJournalView.as_view(),
+                        name='student-results-view'))
+router.add_api_view("Get student classes for semester",
+                    url(r'^get_students_disciplines/$',
+                        apiv1s.ListOfDisciplinesView.as_view(),
+                        name='student-disciplines-view'))
+router.add_api_view("Get Faculty group structure",
+                    url(r'^get_faculties_structure/$',
+                        apiv1s.ListFacultyView.as_view(),
+                        name='faculty-structure-view'))
--- studs/urls.py
+++ studs/urls.py
@@ -16,44 +16,21 @@ Including another URLconf
 from .settings import MEDIA_ROOT, DEBUG
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
+from .routers import router
 
-from rest_framework import routers
-from students.api import apiv1 as apiv1s
-from professors.api import  apiv1 as apiv1p
-from blog.api import apiv1 as apiv1b
-
-
-router = routers.DefaultRouter()
-router.register(r'users', apiv1s.UserViewSet)
-router.register(r'groups', apiv1s.GroupViewSet)
-router.register(r'students', apiv1s.StudentViewSet)
-router.register(r'blog', apiv1b.BlogViewSet)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # API LOGIN FOR USER TO GET TOKEN
-    url(r'^api/v1/auth/login/', apiv1s.LoginAPIView.as_view(), name='login'),
-    # API REGISTER NEW USER
-    url(r'^api/v1/auth/register/', apiv1s.RegisterAPIView.as_view(), name='register'),
-    # API GET USER SCHEDULE FOR TODAY
-    url(r'^api/v1/get_user_schedule/$', apiv1s.TodayScheduleView.as_view(), name='TodaySchedule'),
-    # API GET USER SCHEDULE FOR CURRENT WEEK
-    url(r'^api/v1/get_user_weekly_schedule/$', apiv1s.WeeklyScheduleView.as_view(), name='WeeklySchedule'),
-    # API GET STUDENT GROUP LIST FOR GIVEN GROUP
-    url(r'^api/v1/get_students_group_list/$', apiv1s.GroupStudentListView.as_view(), name='StudentsGroupList'),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     # POST/PUT STUDENT JOURNAL ITEM (PROFESSORS ONLY)
-    url(r'^api/v1/post_st_journal/$', apiv1p.StudentJournalInstanceView.as_view(), name='Journal'),
-    # GET STUDENT DISCIPLINE RESULT BY DATA RANGE
-    url(r'^api/v1/get_st_dsp_result/$', apiv1s.StudentClassJournalView.as_view(), name='Journal1'),
+    #url(r'^api/v1/post_st_journal/$', apiv1p.StudentJournalInstanceView.as_view(), name='Journal'),
+
     # GET GROUPS THAT PROFESSOR IS TEACHING IN THIS SEMESTER
-    url(r'^api/v1/get_teaching_groups/$', apiv1p.GroupsListView.as_view(), name='GroupsListForProfessor'),
-    # GET DISCIPLINES THAT STUDENT USER IS VISITING IN THIS SEMESTER
-    url(r'^api/v1/get_students_disciplines/$', apiv1s.ListOfDisciplinesView.as_view(), name='ListOfStDisciplines'),
-    # GET FACULTY STRUCTURE
-    url(r'^api/v1/get_faculties_structure/', apiv1s.ListFacultyView.as_view(), name='faculty-structure'),
+    #url(r'^api/v1/get_teaching_groups/$', apiv1p.GroupsListView.as_view(), name='GroupsListForProfessor'),
+    url(r'^docs/', include('rest_framework_docs.urls')),
 
 ]
 
