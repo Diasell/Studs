c17c41f38914dd5be71a2d2dbbaecc648c7e8222
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-09-05 00:38:10 +0300

    deleted old commented code

--- professors/api/apiv1.py
+++ professors/api/apiv1.py
@@ -1,6 +1,4 @@
 import datetime
-# from datetime import timedelta
-# from django.contrib.auth import authenticate
 
 from rest_framework import status
 
@@ -9,21 +7,11 @@ from django.contrib.auth.models import User
 
 from rest_framework.authentication import (
     TokenAuthentication,
-    # BasicAuthentication,
-
 )
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
-# from rest_framework import viewsets
-# from rest_framework.authtoken.models import Token
-# from students.api.serializers import (
-#     UserSerializer,
-#     GroupSerializer,
-#     ProfileSerializer,
-# )
 
 from department.models import (
     Disciplines,
