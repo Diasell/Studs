b692a2a953795cc64e22004dc1fbb57f5a3876e4
Taras <gutsalyuk.taras@gmail.com>
2016-11-15 01:21:04 +0200

    test commit for chatbot(url / added)

--- studs/routers.py
+++ studs/routers.py
@@ -67,4 +67,4 @@ router.add_api_view("Get current groups that professor is teaching",
 chatbot_router.add_api_view("send chat bot message",
                     url(r'^test$',
                         apiv1s.TestChatBotView.as_view(),
-                        name='test'))
+                        name='test/'))
